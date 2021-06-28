34d9b06f26e6c39673f615bfa64a709f78861f5e
ishaanjainlock10 <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-28 16:29:34 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -57,4 +57,4 @@ async def on_message(message):
         await message.channel.send("{} likes little schoolgirls >:(".format(message.author))
     if message.content.startswith('+sin'):
         await message.channel.send('aapko apne paapon ka pashchatap karna chahiye| vah tumhe nasht kar denge|aap koi mauka nahi khade hai| tum usko haath par bhuktoge| pachtana|')
-client.run('ODU3NDY3MjIyODM3NTU5Mjk2.YNQAlA.GUgQVCF_B0bRke42CVWdVqc3q-U')
+client.run('ODU3NDY3MjIyODM3NTU5Mjk2.YNQAlA.mrgcpm3L19txhRiqXhlwmiIQvSg')
