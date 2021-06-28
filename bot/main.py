fc6b65aeb885a500cdf4bb9ebf13e65fc0b84b68
ishaanjainlock10 <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-28 22:23:40 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -49,7 +49,7 @@ async def on_message(message):
     if message.content.startswith('+loli'):
         await message.channel.send("{} likes little schoolgirls >:(".format(message.author))
     if message.content.startswith('+spamtest'):
-        await message.channel.send("hi <@dororo9523>")
+        await message.channel.send("hi"+ <@dororo9523>)
     if message.content.startswith('+sin'):
         await message.channel.send('aapko apne paapon ka pashchatap karna chahiye| vah tumhe nasht kar denge|aap koi mauka nahi khade hai| tum usko haath par bhuktoge| pachtana|')
 client.run("ODU3NDY3MjIyODM3NTU5Mjk2.YNQAlA.N8wDKWubrCDivSnZemT_08nPujA")
