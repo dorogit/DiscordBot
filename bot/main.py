20a58b81553539c3bfaa5a6460f2c8cfec5e54cf
ishaanjainlock10 <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-28 17:50:16 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -40,6 +40,8 @@ async def on_message(message):
 
     if message.content.startswith('+owo'):
         await message.channel.send('uwu')
+    if message.content.startswith('+flip'):
+        await message.channel.send('(╯°□°）╯︵ ┻━┻')
     if message.content.startswith('+aho'):
         await message.channel.send('whomst has summoned the almighty one')
     if message.content.startswith('+food'):
