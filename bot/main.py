9e33ffa57ef9b0ad150b005aa8040eccded49544
Ishaan <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-29 11:28:21 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -64,7 +64,8 @@ async def on_message(message):
       list = message.content.split()
       msg = list[1]
       for num in range(50):
-        await message.channel.send(msg)
+        spam +=msg
+      await message.channel.send(spam)
     if message.content.startswith('+sin'):
         await message.channel.send('aapko apne paapon ka pashchatap karna chahiye| vah tumhe nasht kar denge|aap koi mauka nahi khade hai| tum usko haath par bhuktoge| pachtana|')
 client.run("ODU3NDY3MjIyODM3NTU5Mjk2.YNQAlA.N8wDKWubrCDivSnZemT_08nPujA")
