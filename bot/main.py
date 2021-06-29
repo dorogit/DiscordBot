78b3b378c861e16e07ebda1c01573c5be1312f47
Ishaan <84321539+ishaanjainlock10@users.noreply.github.com>
2021-06-29 11:50:17 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -52,14 +52,14 @@ async def on_message(message):
         await message.channel.send(msg)
     if message.content.startswith('+commands'):
         await message.channel.send('only these commands are available :')
-        await message.channel.send('+hello,+qt,+tensai (this one is really cool),+ID,+help,+who,+owo,+food,+gae,+commands,+spamuser @user,+qtmode,+aho, +recipe, +sin,+loli,+flip,+ .... YET')
+        await message.channel.send('+hello,+qt,+tensai (this one is really cool),+ID,+help,+who,+owo,+food,+gae,+commands,+spam @user,+qtmode,+aho, +recipe, +sin,+loli,+flip,+ .... YET')
     if message.content.startswith('+qtmode'):
         await message.channel.send('qt mode activated uwu >.<')
     if message.content.startswith('+recipe'):
         await message.channel.send("Biryani: A mild, rice-based dish cooked with meat and/or vegetables and served with yoghurty sauce Chaat: Savoury snacks, often made with potato or chick peas Dahl: A lentil curry similar to thick lentil soup Elaichi: Cardamom Firni: Rice pudding with saffron and nuts Gosht: Meat, usually lamb Hari Mirch: Green chilli Idli: A savoury lentil cake Jalfrezi: A spicy dish cooked with ginger and chilli Kachori: Crisp pastry rounds with spiced mung dahl or pea filling Lassi: A yoghurt drink, ordered with salt or sugar, sometimes with fruit. Ideal to cool a fiery palate Murgh: Chicken Naan: Flatbreads cooked in a large tandoor oven Onion bhaji: Crispy deep-fried onion balls Peshwari: Bread or dish with sweet dry fruit and nuts Quinoa Pilaf: A spicy Indian preparation of the seed Rasmalai: A traditional dessert of sweet cottage cheese dumplings, topped with saffron Saslik: Anything grilled Tandoori: Baked in a clay/mud oven Udrak: Ginger Vindaloo: Hot curry, often with potato. Xacuti: A Goan dish made with lamb or chicken, coconut and a complex mix of roasted then ground spices Yakhini: Soup or gravy Zeera: Cumin")
     if message.content.startswith('+loli'):
         await message.channel.send("{} likes little schoolgirls >:(".format(message.author))
-    if message.content.startswith('+spamuser'):
+    if message.content.startswith('+spam'):
       await message.channel.send("git updated and pushed to heroku 2")
       list = message.content.split()
       msg = list[1]
