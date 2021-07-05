41e59834817bdc13583c89c4c7224622e27ece83
Ishaan <84321539+ishaanjainlock10@users.noreply.github.com>
2021-07-05 12:21:16 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -17,7 +17,7 @@ async def on_message(message):
     user = message.author
     user1 = None
     myid = None
-    spam = ""
+    spam = 1
     list = []
     a = None
     if message.author == client.user:
