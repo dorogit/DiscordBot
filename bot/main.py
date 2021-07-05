1f44123afb8f2c273c320b707bedc479c5bd69e8
Ishaan <84321539+ishaanjainlock10@users.noreply.github.com>
2021-07-05 12:44:30 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -89,7 +89,7 @@ async def on_message(message):
     if message.content.startswith('+loli'):
         await message.channel.send("{} likes little schoolgirls >:(".format(message.author))
     if message.content.startswith('+spam'):
-      message.channel.send(spam)
+      message.channel.send("working")
       msg = message.content
       if spam < 5:
         for num in range(10):
